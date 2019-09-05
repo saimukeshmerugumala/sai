365a3f677f94a5b25781927b55d417f2589c16bd
saimukeshmerugumala <saimukesh2444@gmail.com>
2019-09-05 13:50:45 +0000

    second

---
+++ ec2-machines.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "phpapp1" {
+  ami                         = "${var.ami}"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = "true"
+  subnet_id                   = "${aws_subnet.PublicAZA.id}"
+  vpc_security_group_ids      = ["${aws_security_group.WebServer.id}"]
+  key_name                    = "${var.key_name}"
+
+  tags = {
+    Name = "WebApp1"
+  }
+
+  user_data = "${file("app-userdata.sh")}"
+}
+
+resource "aws_instance" "phpapp2" {
+  ami                         = "${var.ami}"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = "true"
+  subnet_id                   = "${aws_subnet.PublicAZB.id}"
+  vpc_security_group_ids      = ["${aws_security_group.WebServer.id}"]
+  key_name                    = "${var.key_name}"
+
+  tags = {
+    Name = "WebApp2"
+  }
+
+  user_data = "${file("app-userdata.sh")}"
+}
